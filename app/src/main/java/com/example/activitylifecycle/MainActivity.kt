71cd1f7a58a1822125566dbf6c20efda3cab1148
bvogler10 <brinjavogler@gmail.com>
2024-02-15 11:35:16 -0500

    activity lifecycle logging and observations

---
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.activitylifecycle
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "ActivityLifecycle"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
+
+        val button: Button = findViewById<Button>(R.id.button)
+        val textView: TextView = findViewById(R.id.textView)
+        val editText: EditText = findViewById(R.id.editText)
+
+        button.setOnClickListener {
+            textView.text = "Hello"
+            editText.setText("Hello")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+}
